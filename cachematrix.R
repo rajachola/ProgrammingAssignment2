a45a0638cbc3ef43338b3293c90685009b0505ab
rajachola <raja.derkaiser@gmail.com>
2015-10-25 18:28:56 +0530

    Programming Assignment 2
    commit to branch

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Take advantage of the scoping rules of the R language
+## to preserve state inside of an R object.
+## <<- operator is used to set variable value outside the 
+## scope of function
 
-## Write a short comment describing this function
-
+## This function creates a special "matrix" object
+## whose inverse can be computed
 makeCacheMatrix <- function(x = matrix()) {
-
+            m <- NULL
+            set <- function(y) {
+                    x <<- y
+                    m <<- NULL
+            }
+            get <- function() x
+            setinverse <- function(inverse) m <<- inverse
+            getinverse <- function() m
+            list(set = set, get = get,
+                 setinverse = setinverse,
+                 getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## checks if matrix inverse is not already computed
+# if not function computes the inverse
+# else returns the cached matrix inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		m <- x$getinverse()
+            if(!is.null(m)) {
+                    message("getting cached data")
+                    return(m)
+            }
+            data <- x$get()
+            m <- solve(data, ...)
+            x$setinverse(m)
+            m
 }
